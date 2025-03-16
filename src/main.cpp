a9a0580336274deb9f075e169cfed02f450aac93
Alex Banning <banninghg80@gmail.com>
2025-03-15 23:25:39 -0500

    Link glfw

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
     return 0;
 }
 
