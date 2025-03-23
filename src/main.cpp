8ba91d4360d8054d45279ee7675743f9f5db55b7
Alex Banning <banninghg80@gmail.com>
2025-03-23 14:23:24 -0500

    rotate around scene

--- src/main.cpp
+++ src/main.cpp
@@ -155,7 +155,7 @@ int main() {
         glm::vec3( 1.5f,  0.2f, -1.5f), 
         glm::vec3(-1.3f,  1.0f, -1.5f)  
     };
-    while (!glfwWindowShouldClose(window))
+   while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
@@ -172,7 +172,12 @@ int main() {
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
         glm::mat4 view = glm::mat4(1.0f);
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+ 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
         int modelLoc = glGetUniformLocation(ourShader.ID, "model");
