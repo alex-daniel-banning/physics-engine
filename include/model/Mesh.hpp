68716be4fd42b12dfd695830e47fe460129fe02a
Alex Banning <banninghg80@gmail.com>
2025-04-06 17:46:12 -0500

    clean up model classes

--- include/model/Mesh.hpp
+++ include/model/Mesh.hpp
@@ -1,21 +1,24 @@
 #ifndef MESH_H
 #define MESH_H
 #include <vector>
-#include "Vertex.hpp"
-#include "Texture.hpp"
+
 #include "Shader.hpp"
+#include "Texture.hpp"
+#include "Vertex.hpp"
 
 class Mesh {
-    public:
-        std::vector<Vertex>       vertices;
-        std::vector<unsigned int> indices;
-        std::vector<Texture>      textures;
+public:
+  std::vector<Vertex> vertices;
+  std::vector<unsigned int> indices;
+  std::vector<Texture> textures;
 
-        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
-        void Draw(Shader &Shader);
-    private:
-        unsigned int VAO, VBO, EBO;
-        void setupMesh();
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+       std::vector<Texture> textures);
+  void Draw(Shader &Shader);
+
+private:
+  unsigned int VAO, VBO, EBO;
+  void setupMesh();
 };
 
 #endif
--- include/model/WorldObject.hpp
+++ include/model/WorldObject.hpp
@@ -1,18 +1,18 @@
 #ifndef WORLD_OBJECT_H
 #define WORLD_OBJECT_H
 #include <glm/glm.hpp>
-#include <model/Model.hpp>
+
+#include "model/Model.hpp"
 
 class WorldObject {
-    public:
-        // Builder pattern?
-        WorldObject(std::string const &path);
-        void Draw(Shader &shader);
-    private:
-        // todo, refactor to add m_ prefix
-        Model model;
-        glm::vec3 position = glm::vec3(0.0f);
-        glm::vec3 m_scale = glm::vec3(1.0f);
+public:
+  WorldObject(std::string const &path);
+  void Draw(Shader &shader);
+
+private:
+  Model m_model;
+  glm::vec3 m_position = glm::vec3(0.0f);
+  glm::vec3 m_scale = glm::vec3(1.0f);
 };
 
 #endif
