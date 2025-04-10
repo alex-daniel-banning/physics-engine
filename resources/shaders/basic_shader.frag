37abd9353012adecac0165a436ac99389cfaf643
Alex Banning <banninghg80@gmail.com>
2025-04-10 18:48:57 -0500

    texture is working, shadows are still WIP

--- resources/shaders/basic_shader.frag
+++ resources/shaders/basic_shader.frag
@@ -3,10 +3,14 @@ out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;
 
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform sampler2D texture_diffuse1;
+uniform bool useTexture;
+
 void main()
 {
    float ambientStrength = 0.1;
@@ -17,6 +21,12 @@ void main()
    float diff = max(dot(norm, lightDir), 0.0);
    vec3 diffuse = diff * lightColor;
 
-   vec3 result = (ambient + diffuse) * objectColor;
+   // sample texture
+   vec3 color = useTexture ? texture(texture_diffuse1, TexCoords).rgb : objectColor;
+
+   // combine lighting and texture
+   vec3 result = (ambient + diffuse) * color;
+
    FragColor = vec4(result, 1.0);
+   //FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,10 @@
 #include "model/WorldObject.hpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+unsigned int loadTexture(char const *path);
 
 // settings
 unsigned int DEFAULT_SCREEN_WIDTH = 1600;
@@ -66,12 +67,17 @@ int main() {
   glFrontFace(
       GL_CCW); // Counterclockwise (CCW) faces are considered front-facing
 
+  unsigned int woodTexture =
+      loadTexture(ProjectRoot::getPath("/resources/container.jpg").c_str());
   Shader basicShader(
       ProjectRoot::getPath("/resources/shaders/basic_shader.vert"),
       ProjectRoot::getPath("/resources/shaders/basic_shader.frag"));
   Shader lightSourceShader(
       ProjectRoot::getPath("/resources/shaders/basic_shader.vert"),
       ProjectRoot::getPath("/resources/shaders/light_source_shader.frag"));
+  Shader simpleDepthShader(
+      ProjectRoot::getPath("/resources/shaders/simple_depth_shader.vert"),
+      ProjectRoot::getPath("/resources/shaders/simple_depth_shader.frag"));
 
   glm::vec3 sphereColor = glm::vec3(0.8f, 0.0f, 0.0f);
 
@@ -84,15 +90,70 @@ int main() {
   WorldObject plane =
       WorldObject(ProjectRoot::getPath("/resources/models/plane/plane.obj"));
   plane.setPosition(glm::vec3(0.0f, -2.0f, 0.0f));
-  plane.setScale(glm::vec3(10.0f));
-  glm::vec3 planeColor = glm::vec3(0.0f, 0.1f, 0.2f);
+  plane.setScale(glm::vec3(100.0f));
+  glm::vec3 planeColor = glm::vec3(0.0f, 0.1f, 0.1f);
   camera.lookAt(worldObject.getPosition());
+
+  /* Shadow stuff */
+  unsigned int depthMapFBO;
+  glGenFramebuffers(1, &depthMapFBO);
+
+  const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+  unsigned int depthMap;
+  glGenTextures(1, &depthMap);
+  glBindTexture(GL_TEXTURE_2D, depthMap);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH,
+               SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D,
+                         depthMap, 0);
+  glDrawBuffer(GL_NONE);
+  glReadBuffer(GL_NONE);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
   while (!glfwWindowShouldClose(window)) {
     float currentFrame = glfwGetTime();
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
     processInput(window);
+
+    /*
+    // 1. first render to depth map
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    ConfigureShaderAndMatrices();
+    RenderScene();
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    // 2. then render scene as normal with shadow mapping (using depth map)
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    ConfigureShaderAndMatrices();
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    RenderScene();
+    */
+
+    /*
+    float near_plane = 1.0f, far_plane = 7.5f;
+    glm::mat4 lightProjection =
+        glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+    glm::mat4 lightView =
+        glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+    simpleDepthShader.use();
+    simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    worldObject.Draw(simpleDepthShader);
+    plane.Draw(simpleDepthShader);
+    /* End shadow stuff */
 
     // rendering commands here
     glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
@@ -104,10 +165,20 @@ int main() {
     basicShader.setVec3("objectColor", sphereColor);
     basicShader.setVec3("lightColor", lightColor);
     basicShader.setVec3("lightPos", lightPos);
+    basicShader.setBool("useTexture", false);
     worldObject.Draw(basicShader);
 
     basicShader.setVec3("objectColor", planeColor);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, woodTexture);
+    basicShader.setInt("texture_diffuse1", 0);
+    basicShader.setBool("useTexture", true);
     plane.Draw(basicShader);
+    // glBindTexture(GL_TEXTURE_2D, 0);
+    //  GLenum err;
+    //  while ((err = glGetError()) != GL_NO_ERROR) {
+    //    std::cout << "OpenGL error: " << err << std::endl;
+    //  }
 
     lightSourceShader.use();
     lightSourceShader.setMat4("projection", camera.getProjection());
@@ -169,7 +240,7 @@ void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
   float yoffset = camera.getLastY() - ypos;
   camera.setLastMousePos(xpos, ypos);
 
-  const float sensitivity = 0.05f;
+  const float sensitivity = 0.03f;
   xoffset *= sensitivity;
   yoffset *= sensitivity;
   camera.setYaw(camera.getYaw() + xoffset);
@@ -195,4 +266,40 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
     camera.setFov(1.0f);
   if (camera.getFov() > 45.0f)
     camera.setFov(45.0f);
+}
+
+unsigned int loadTexture(char const *path) {
+  unsigned int textureID;
+  glGenTextures(1, &textureID);
+
+  int width, height, nrComponents;
+  unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+  if (data) {
+    GLenum format;
+    if (nrComponents == 1)
+      format = GL_RED;
+    else if (nrComponents == 3)
+      format = GL_RGB;
+    else if (nrComponents == 4)
+      format = GL_RGBA;
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                    format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                    format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(data);
+  } else {
+    std::cout << "Texture failed to load at path: " << path << std::endl;
+    stbi_image_free(data);
+  }
+  return textureID;
 }
